8576e05c577c09f3d6b2c874b23b3c0ca41577e3
Yucan Liu <yucanliu@dyn-160-39-247-130.dyn.columbia.edu>
2017-11-15 21:17:01 -0500

    data added

--- testScript/yahoodownload.py
+++ testScript/yahoodownload.py
@@ -1,1 +1,16 @@
-import 
+import fix_yahoo_finance as yf
+import csv
+
+sp500 = csv.reader(open("sp500"), delimiter=",")
+
+for row in sp500:
+    print row
+
+for x in row:
+    print x
+    data = yf.download(x)
+    print "Data Size = " + str(data.size) + "\n"
+    if data.size != 0:
+        data.to_csv('data/'+x+'.csv')
+    else:
+        print "NULL DATA"
