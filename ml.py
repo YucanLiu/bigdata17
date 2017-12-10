c7fd9be8618cbd44eec8194d7f62e27bb030ed7c
Yuxin <yd2406@columbia.edu>
2017-12-10 15:08:21 -0500

    Added Kmeans+Regressor method

--- ml.py
+++ ml.py
@@ -30,32 +30,8 @@ def import_data( ):
 
 
 #Testing data row 126 364 409 bad data
-def cluster_test(train_data, test_data):
-    t_data = []
-    for x in range (0, len(train_data)):
-        t_data.append(np.asarray(train_data[x][: len(train_data[0]) - d]))
-    train_data = np.asarray(t_data)
-    # print(len(train_data[0]))
+def cluster_test(train_data,test_data):
 
-    # print("original test data")
-    # print(test_data.shape)
-
-    test_data = test_data[0][d:]
-    temp_data = []
-    temp_data.append(test_data)
-    test_data = np.asarray(temp_data)
-    # print(len(test_data))
-    # print("final test data")
-    # print(test_data.shape)
-
-
-    ### DEBUGGING
-    # print(train_data.shape)
-    # print(test_data.shape)
-
-    ######K-means
-    #data = np.concatenate((test_data,train_data))
-    #print(data)
     clf = KMeans(n_clusters=n_c, max_iter=iters, algorithm='full')
     clf = clf.fit(train_data)
     train_label = clf.predict(train_data)
@@ -69,26 +45,29 @@ def cluster_test(train_data, test_data):
             corr_test_data.append(train_data[i])
     print("total number of relatives: " + str(len(corr_test_data)))
 
-    corr_test_data=np.asarray(corr_test_data)
-    plt_data=np.concatenate((corr_test_data,test_data))
+    corr_test_data = np.asarray(corr_test_data)
 
-    for x in range(len(plt_data)):
-        for y in range(len(plt_data[0])):
-            if plt_data[x][y] >= 0:
-                plt_data[x][y] = 1
-            else:
-                plt_data[x][y] = -1
+    LassoRegression(corr_test_data.transpose(),test_data.transpose())
+
+    # plt_data=np.concatenate((corr_test_data,test_data))
+
+    # for x in range(len(plt_data)):
+    #     for y in range(len(plt_data[0])):
+    #         if plt_data[x][y] >= 0:
+    #             plt_data[x][y] = 1
+    #         else:
+    #             plt_data[x][y] = -1
     # print(plt_data)
 
-    sum=0
-    for x in range(len(plt_data)):
-        accuracy = np.mean(plt_data[x] == plt_data[len(plt_data)-1])
-        if(accuracy != 1.0):
-            sum+=accuracy
-        # print(accuracy)
+    #sum=0
+    #for x in range(len(plt_data)):
+    #    accuracy = np.mean(plt_data[x] == plt_data[len(plt_data)-1])
+    #    if(accuracy != 1.0):
+    #        sum+=accuracy
+    #    # print(accuracy)
     # print(sum)
-    print('average accuracy:',sum/(len(plt_data)-1))
-    return sum/(len(plt_data)-1)
+    # print('average accuracy:',sum/(len(plt_data)-1))
+    # return sum/(len(plt_data)-1)
 
     #import matplotlib.pyplot as mpl
     # x = np.asarray([range(len(corr_test_data))])
@@ -98,8 +77,8 @@ def cluster_test(train_data, test_data):
 
     #mpl.show()
 
-def RidgeRegression():
-    mae1 = 0
+def RidgeRegression(train_data, train_label):
+    accuracy1 = 0
     for i in range(len(train_data) - d - 240):
         X_train = train_data[i : 240+i]
         Y_train = train_label[i+d : 240+i+d]
@@ -109,12 +88,13 @@ def RidgeRegression():
         reg1 = Ridge()
         reg1 = reg1.fit(X_train, Y_train)
         predict1 = reg1.predict(X_test)
-        mae1 = mae1 + np.absolute(Y_test - predict1)
-    mae1 = mae1 / (i+1)
-    print("Ridge Regressor Mean Absolute Error is %f" % mae1 )
+        if (predict1 <= 0 and Y_test <= 0) or (predict1 > 0 and Y_test > 0):
+            accuracy1 = accuracy1 + 1
+    accuracy1 = accuracy1 / (i+1)
+    print("Ridge Regressor Accuracy is %f" % accuracy1)
 
-def LassoRegression():
-    mae2 = 0
+def LassoRegression(train_data, train_label):
+    accuracy2 = 0
     for i in range(len(train_data) - d - 240):
         X_train = train_data[i : 240+i]
         Y_train = train_label[i+d : 240+i+d]
@@ -124,12 +104,13 @@ def LassoRegression():
         reg2 = Lasso()
         reg2 = reg2.fit(X_train, Y_train)
         predict2 = reg2.predict(X_test)
-        mae2 = mae2 + np.absolute(Y_test - predict2)
-    mae2 = mae2 / (i+1)
-    print("Lasso Regressor Mean absolute error is %f" % mae2)
+        if (predict2 <= 0 and Y_test <= 0) or (predict2 > 0 and Y_test > 0):
+            accuracy2 = accuracy2 + 1
+    accuracy2 = accuracy2 / (i+1)
+    print("Lasso Regressor Accuracy is %f" % accuracy2)
 
-def RandomForestRegreesion():
-    mae3 = 0
+def RandomForestRegreesion(train_data, train_label):
+    accuracy3 = 0
     for i in range(len(train_data) - d - 240):
         X_train = train_data[i : 240+i]
         Y_train = train_label[i+d : 240+i+d]
@@ -140,12 +121,13 @@ def RandomForestRegreesion():
         Y_train1 = np.ravel(Y_train)
         reg3 = reg3.fit(X_train, Y_train1)
         predict3 = reg3.predict(X_test)
-        mae3 = mae3 + np.absolute(Y_test - predict3)
-    mae3 = mae3 / (i+1)
-    print("Random Forest Regressor Mean absolute error is %f" % mae3)
+        if (predict3 <= 0 and Y_test <= 0) or (predict3 > 0 and Y_test > 0):
+            accuracy3 = accuracy3 + 1
+    accuracy3 = accuracy3 / (i+1)
+    print("Random Forest Regressor Accuracy is %f" % accuracy3)
 
-def BoostingRegression():
-    mae4 = 0
+def BoostingRegression(train_data, train_label):
+    accuracy4 = 0
     for i in range(len(train_data) - d - 240):
         X_train = train_data[i : 240+i]
         Y_train = train_label[i+d : 240+i+d]
@@ -156,14 +138,15 @@ def BoostingRegression():
                             reg_alpha=1e-5)
         reg4 = reg4.fit(X_train, Y_train)
         predict4 = reg4.predict(X_test)
-        mae4 = mae4 + np.absolute(Y_test - predict4)
-    mae4 = mae4 / (i+1)
-    print("XGBoost Regressor Mean absolute error is %f " % mae4)
+        if (predict4 <= 0 and Y_test <= 0) or (predict4 > 0 and Y_test > 0):
+            accuracy4 = accuracy4 + 1
+    accuracy4 = accuracy4 / (i + 1)
+    print("XGBoost Regressor Accuracy is %f " % accuracy4)
 
 if __name__=='__main__':
     ###PARAMETER
     d = 5  # date of delays
-    n_c = 400  # number of clusters
+    n_c = 200  # number of clusters
     iters = 5000  # number of iterations
 
     #Row = time, Columns = companies
@@ -171,13 +154,14 @@ if __name__=='__main__':
     train_data = SP500
     train_label = target
 
-    RidgeRegression()
-    LassoRegression()
-    RandomForestRegreesion()
-    BoostingRegression()
+    #Traditional Regressions
+    # RidgeRegression()
+    # LassoRegression()
+    # RandomForestRegreesion()
+    # BoostingRegression()
 
     #Kmeans + Regression
-
+    cluster_test(train_data.transpose(), train_label.transpose())
     #max = 0
     #max_delay = 0
     #for x in range(1,100):
@@ -189,13 +173,6 @@ if __name__=='__main__':
     #        max_delay = x
 
     #print ('\nThe best accuracy is:', max, 'when delay is:', max_delay)
-
-# kernel k means
-# Time series analysis
-# R
-# Random Forest, Gradient Boosting
-# Ridge Regression, Lasso Regression
-# Compairsion
 
 # Tecniques
 # Bussiness
