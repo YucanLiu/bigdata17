8f2f92b545d2f73d69f9b4d2ddc7a033bedf9411
Yuxin <yd2406@columbia.edu>
2017-12-13 13:13:52 -0500

    Modified ml.py and Add result

--- ml.py
+++ ml.py
@@ -5,49 +5,62 @@ from sklearn.linear_model import Lasso, Ridge
 from sklearn.ensemble import RandomForestRegressor
 from xgboost import XGBRegressor
 
-train_data_path = 'test1/samples.csv'
-test_data_path = 'test1/target.csv'
+train_data_path = 'data_all/output.csv'
+# train_data_path = 'test1/samples.csv'
+test_data_path = 'data_all/BABA.csv'
 
 #import data
 def import_data( ):
+    # Rows: Days; Columns: Companies => 993 * 998
     train_data = []
     test_data = []
     with open(train_data_path) as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=',')
-        for row in spamreader:
-            row = [float(x) for x in row]
-            train_data.append(row)
-    train_data = np.asarray(train_data).transpose()
-    # print(train_data)
+        datareader = csv.reader(csvfile, delimiter=',', strict = True)
+        i = 0
+        for row in datareader:
+            if(i != 0):
+                row = [float(x) for x in row if x != '']
+                train_data.append(row)
+            i += 1
+    train_data = np.asarray(train_data) #997 days * 998 companies
 
     with open(test_data_path) as csvfile:
         spamreader = csv.reader(csvfile, delimiter=',', strict=True)
         for row in spamreader:
             row = [float(x) for x in row]
             test_data.append(row)
-    test_data = np.asarray(test_data)
+    test_data = np.asarray(test_data) # 814 days
+    # print(len(test_data))
+    #testing data is a matrix (days * companies)
+    #training data is a cloumn
     return train_data, test_data
 
 
-#Testing data row 126 364 409 bad data
 def cluster_test(train_data,test_data):
-
     clf = KMeans(n_clusters=n_c, max_iter=iters, algorithm='full')
-    clf = clf.fit(train_data)
-    train_label = clf.predict(train_data)
+    temp_train_data = train_data[183-d:997-d].transpose()
+    # print(len(temp_train_data))
+    # print(len(temp_train_data[0]))
+    clf = clf.fit(temp_train_data)
+    train_label = clf.predict(temp_train_data)
     # print(train_label)
     test_label = clf.predict(test_data)
     # print(test_label)
 
+    train_data = train_data.transpose()
     corr_test_data = []
     for i in range(len(train_data)):
         if train_label[i] == test_label[0]:
             corr_test_data.append(train_data[i])
-    print("total number of relatives: " + str(len(corr_test_data)))
+    # print("total number of relatives: " + str(len(corr_test_data)))
 
     corr_test_data = np.asarray(corr_test_data)
 
+    print("KMeans + Regression Result: ")
+    RidgeRegression(corr_test_data.transpose(),test_data.transpose())
     LassoRegression(corr_test_data.transpose(),test_data.transpose())
+    RandomForestRegreesion(corr_test_data.transpose(),test_data.transpose())
+    BoostingRegression(corr_test_data.transpose(),test_data.transpose())
 
     # plt_data=np.concatenate((corr_test_data,test_data))
 
@@ -79,11 +92,11 @@ def cluster_test(train_data,test_data):
 
 def RidgeRegression(train_data, train_label):
     accuracy1 = 0
-    for i in range(len(train_data) - d - 240):
-        X_train = train_data[i : 240+i]
-        Y_train = train_label[i+d : 240+i+d]
-        X_test = train_data[240+i : 240+i+1]
-        Y_test = train_label[240+d : 240+d+1]
+    for i in range(553):
+        X_train = train_data[183 + i - d : 183 + 260 + i - d]
+        Y_train = train_label[i : 260 + i]
+        X_test = train_data[183 + 260 + i - d : 183 + 261 + i - d]
+        Y_test = train_label[260 + i : 261 + i]
 
         reg1 = Ridge()
         reg1 = reg1.fit(X_train, Y_train)
@@ -95,11 +108,11 @@ def RidgeRegression(train_data, train_label):
 
 def LassoRegression(train_data, train_label):
     accuracy2 = 0
-    for i in range(len(train_data) - d - 240):
-        X_train = train_data[i : 240+i]
-        Y_train = train_label[i+d : 240+i+d]
-        X_test = train_data[240+i : 240+i+1]
-        Y_test = train_label[240+d : 240+d+1]
+    for i in range(553):
+        X_train = train_data[183 + i - d : 183 + 260 + i - d]
+        Y_train = train_label[i : 260 + i]
+        X_test = train_data[183 + 260 + i - d : 183 + 261 + i - d]
+        Y_test = train_label[260 + i : 261 + i]
 
         reg2 = Lasso()
         reg2 = reg2.fit(X_train, Y_train)
@@ -111,11 +124,11 @@ def LassoRegression(train_data, train_label):
 
 def RandomForestRegreesion(train_data, train_label):
     accuracy3 = 0
-    for i in range(len(train_data) - d - 240):
-        X_train = train_data[i : 240+i]
-        Y_train = train_label[i+d : 240+i+d]
-        X_test = train_data[240+i : 240+i+1]
-        Y_test = train_label[240+d : 240+d+1]
+    for i in range(553):
+        X_train = train_data[183 + i - d : 183 + 260 + i - d]
+        Y_train = train_label[i : 260 + i]
+        X_test = train_data[183 + 260 + i - d : 183 + 261 + i - d]
+        Y_test = train_label[260 + i : 261 + i]
 
         reg3 = RandomForestRegressor()
         Y_train1 = np.ravel(Y_train)
@@ -128,11 +141,11 @@ def RandomForestRegreesion(train_data, train_label):
 
 def BoostingRegression(train_data, train_label):
     accuracy4 = 0
-    for i in range(len(train_data) - d - 240):
-        X_train = train_data[i : 240+i]
-        Y_train = train_label[i+d : 240+i+d]
-        X_test = train_data[240+i : 240+i+1]
-        Y_test = train_label[240+d : 240+d+1]
+    for i in range(553):
+        X_train = train_data[183 + i - d : 183 + 260 + i - d]
+        Y_train = train_label[i : 260 + i]
+        X_test = train_data[183 + 260 + i - d : 183 + 261 + i - d]
+        Y_test = train_label[260 + i : 261 + i]
 
         reg4 = XGBRegressor(max_depth=4, min_child_weight=4, gamma=0.4, subsample=0.6, colsample_bytree=0.6,
                             reg_alpha=1e-5)
@@ -145,23 +158,27 @@ def BoostingRegression(train_data, train_label):
 
 if __name__=='__main__':
     ###PARAMETER
-    d = 5  # date of delays
-    n_c = 200  # number of clusters
+    delay = [5, 20, 40, 60, 100, 120]  # date of delays
+    n_c = 400  # number of clusters
     iters = 5000  # number of iterations
 
     #Row = time, Columns = companies
-    SP500, target = import_data( )
-    train_data = SP500
+    samples, target = import_data( )
+    train_data = samples
     train_label = target
 
-    #Traditional Regressions
-    # RidgeRegression()
-    # LassoRegression()
-    # RandomForestRegreesion()
-    # BoostingRegression()
+    for d in delay:
+        print('Delay Days: %d' % d)
+        #Traditional Regressions
+        RidgeRegression(train_data, train_label)
+        LassoRegression(train_data, train_label)
+        RandomForestRegreesion(train_data, train_label)
+        BoostingRegression(train_data, train_label)
 
-    #Kmeans + Regression
-    cluster_test(train_data.transpose(), train_label.transpose())
+        #Kmeans + Regression
+        cluster_test(train_data, train_label.transpose())
+        print(' ')
+
     #max = 0
     #max_delay = 0
     #for x in range(1,100):
@@ -174,6 +191,6 @@ if __name__=='__main__':
 
     #print ('\nThe best accuracy is:', max, 'when delay is:', max_delay)
 
-# Tecniques
+# Techniques
 # Bussiness
 # Big Data Aspect
